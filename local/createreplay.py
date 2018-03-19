78ec277fce5487178f8c09376bf80bbd0480415b
Amzza0x00 <32904523+Amzza0x00@users.noreply.github.com>
2018-03-19 16:39:40 +0800

    Update createreplay.py

--- local/createreplay.py
+++ local/createreplay.py
@@ -1,17 +1,17 @@
 
 
 def create(targetip, savefile):
-    with open(".\\payloads\\client.bin", "rb") as f:
+    with open(".\\payloads\\remote.bin", "rb") as f:
         with open(savefile, "wb") as fp:
             for i in f:
-                fp.write(i.replace(b"10.10.1.115", targetip.encode()))
+                fp.write(i.replace(b"10.10.1.115", targetip.encode()))  # bug: 替换ip地址后文件损坏
 
     print("[*] 文件构建在%s" % savefile)
 
 
 def repair():
-    with open(".\\payloads\\client.exe", "rb") as f:
-        with open(".\\payloads\\client.bin", "wb") as fp:
+    with open(".\\payloads\\remote.exe", "rb") as f:
+        with open(".\\payloads\\remote.bin", "wb") as fp:
             for i in f:
                 fp.write(i)
 
